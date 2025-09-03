c584f4d70f9aaebda04a932c50d4a9af7fe64747
RayhanaAkterDev <rayhanaaktersumaya.dev@gmail.com>
2025-09-04 00:44:25 +0600

    setup(frontend): initialize React project with Vite and Tailwind v4

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>
+        Hospital Management
+        System 2.0
+      </h1>
+    </>
+  );
+}
+
+export default App;
