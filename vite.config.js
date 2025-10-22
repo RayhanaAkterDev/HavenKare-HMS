9150ca74923d1098d81ac167a5f6c818e9526c7a
RayhanaAkterDev <rayhanaaktersumaya.dev@gmail.com>
2025-10-22 11:10:42 +0600

    trying an experiment so remove all the things till now did

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-	plugins: [tailwindcss(), autoprefixer()],
-	build: {
-		outDir: 'public', // optional if you want build output in 'public'
-	},
-})
